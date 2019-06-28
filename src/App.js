cf6702eb47a80c7b7cd0c0f56a41b40ed6ddcdd6
Javontay McElroy <javontaymcelroy@Javontays-MBP.attlocal.net>
2019-06-28 18:05:14 -0500

    shifted files around.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Hercules</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
